95d3d6aed9b8c566c629984d2872951ca0f35b91
warda <warda2521@hotmail.com>
2023-03-04 14:02:28 +0000

    updated utils.py

--- custom-vertex-ai-pipelines/src/utils/utils.py
+++ custom-vertex-ai-pipelines/src/utils/utils.py
@@ -3,23 +3,6 @@ import io
 from google.cloud import storage
 import base64
 
-IMAGE_PATH = 'images/test.png'
-
-
-def save_fig_to_bucket(bucket_name:str, image_path:str=IMAGE_PATH):
-    """Saves a png image file to GCP Storage Bucket"""
-    fig_to_upload = plt.gcf()
-
-    # save figure image to a bytes buffer
-    buf = io.BytesIO()
-    fig_to_upload.savefig(buf, format='png')
-
-    # init GCS client and upload buffer contents
-    client = storage.Client()
-    bucket = client.get_bucket(bucket_name)
-    blob = bucket.blob(image_path)  
-    blob.upload_from_file(buf, content_type='image/png', rewind=True)
-
 
 def get_image_data():
     """Gets binary data url for a matplotlib figure"""
